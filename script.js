ada4cf7959e399d55a28c992c9f2dd49c719cee7
AubreyBennett <27aubreyb@gmail.com>
2021-01-18 18:53:57 -0500

    setting color blocking and local storage with jquery

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+console.log(todayDate);
 
-$(document).ready(function () {
-    var currentHour = moment().hours();
-    var todayDate = moment().format("dddd, MMMM Do YYYY");
-    console.log(currentHour);
-    console.log(todayDate);
+$(document).ready(function() {
+    $('.time-block').each(function() {
+        $(this).attr('hour');
+        var hour = $(this).attr('hour');
+        hour = moment().hour(hour);
+        var currentTime = moment();
+        console.log(hour);
+        if (currentTime.hour() === hour) {
+            $(this).children('textarea').addClass('present');
+        }
+        if (currentTime.hour() < hour) {
+            $(this).children('textarea').addClass('past');
+        }
+        if (currentTime.hour() > hour) {
+            $(this).children('textarea').addClass('future');
+        }
+    var something = localStorage.getItem(hour.hour());
+    $(this).children('textarea').val(something);
+    console.log(something);
+    console.log(hour);
+    });
+    $('.time-block').children('button').click(function () {
+        var hour = $(this).parent().attr('hour');
+        var text = $(this).prev().val();
+
+        localStorage.setItem(hour, text);
+    });
 });
 
+
+// past present and future color blocking
+
+// when I click the save button it saves to local storage
