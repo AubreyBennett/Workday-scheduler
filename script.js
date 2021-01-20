0e3eef6ecb7461f5c33b44ca618deacbd6d97e7b
AubreyBennett <27aubreyb@gmail.com>
2021-01-19 21:55:26 -0500

    cleanup code

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// the current day is displayed at the top of the calendar
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 console.log(todayDate);
-
+// time blocks with color coding
 $(document).ready(function() {
     $('.time-block').each(function() {
         $(this).attr('hour');
@@ -18,11 +19,13 @@ $(document).ready(function() {
         if (currentTime.hour() > hour) {
             $(this).children('textarea').addClass('future');
         }
+    // local storage
     var something = localStorage.getItem(hour.hour());
     $(this).children('textarea').val(something);
     console.log(something);
     console.log(hour);
     });
+
     $('.time-block').children('button').click(function () {
         var hour = $(this).parent().attr('hour');
         var text = $(this).prev().val();
